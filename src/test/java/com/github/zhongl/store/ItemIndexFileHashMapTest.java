7ec424eb76663fed844d9a0cecda825b382c0451
zhongl <zhong.lunfu@gmail.com>
2011-11-28 14:38:14 +0800

    extract ItemIndexFileHashMap benchmark.

--- src/test/java/com/github/zhongl/store/ItemIndexFileHashMapTest.java
+++ src/test/java/com/github/zhongl/store/ItemIndexFileHashMapTest.java
@@ -1,13 +1,10 @@
 package com.github.zhongl.store;
 
-import com.github.zhongl.store.benchmark.*;
 import com.google.common.io.Files;
 import com.google.common.primitives.Ints;
 import org.junit.Test;
 
 import java.io.IOException;
-import java.util.Collection;
-import java.util.concurrent.Callable;
 
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.nullValue;
@@ -45,30 +42,6 @@ public class ItemIndexFileHashMapTest extends FileBase {
         assertThat(map.put(key, itemIndex), is(nullValue()));
         assertThat(map.remove(key), is(itemIndex));
         assertThat(map.get(key), is(nullValue()));
-    }
-
-    @Test
-    public void benchmark() throws Exception {
-        file = testFile("benchmark");
-        map = newItemIndexFileHashMap(100);
-
-        CallableFactory addFactory = new AddFactory(map);
-        CallableFactory replaceFactory = new ReplaceFactory(map);
-        CallableFactory getFactory = new GetFactory(map);
-        CallableFactory removeFactory = new RemoveFactory(map);
-
-
-        CallableFactory concatCallableFactory = new ConcatCallableFactory(
-                new FixInstanceSizeFactory(100, addFactory),
-                new FixInstanceSizeFactory(100, replaceFactory),
-                new FixInstanceSizeFactory(100, getFactory),
-                new FixInstanceSizeFactory(100, removeFactory)
-        );
-
-        Collection<Statistics> statisticses = new Benchmarker(concatCallableFactory, 1, 400).benchmark();
-        for (Statistics statisticse : statisticses) {
-            System.out.println(statisticse);
-        }
     }
 
     @Test
@@ -138,91 +111,4 @@ public class ItemIndexFileHashMapTest extends FileBase {
         return new ItemIndexFileHashMap(file, initCapacity);
     }
 
-    abstract class OperationFactory implements CallableFactory {
-        protected final ItemIndexFileHashMap map;
-        private int count;
-
-        public OperationFactory(ItemIndexFileHashMap map) {this.map = map;}
-
-        protected byte[] genKey() {
-            return Ints.toByteArray(count++);
-        }
-    }
-
-    private class AddFactory extends OperationFactory {
-
-        public AddFactory(ItemIndexFileHashMap map) {
-            super(map);
-        }
-
-        @Override
-        public Callable<?> create() {
-            return new Callable<Object>() {
-                @Override
-                public Object call() throws Exception {
-                    return map.put(Md5Key.valueOf(genKey()), genItemIndex());
-                }
-            };
-        }
-
-        protected ItemIndex genItemIndex() {
-            return new ItemIndex(0, 0L);
-        }
-
-    }
-
-    private class ReplaceFactory extends AddFactory {
-
-        public ReplaceFactory(ItemIndexFileHashMap map) {
-            super(map);
-        }
-
-        @Override
-        public Callable<?> create() {
-            return new Callable<Object>() {
-                @Override
-                public Object call() throws Exception {
-                    return map.put(Md5Key.valueOf(genKey()), genItemIndex());
-                }
-            };
-        }
-
-
-    }
-
-    private class GetFactory extends OperationFactory {
-
-        public GetFactory(ItemIndexFileHashMap map) {
-            super(map);
-        }
-
-        @Override
-        public Callable<?> create() {
-            return new Callable<Object>() {
-                @Override
-                public Object call() throws Exception {
-                    return map.get(Md5Key.valueOf(genKey()));
-                }
-            };
-        }
-
-    }
-
-    private class RemoveFactory extends OperationFactory {
-
-        public RemoveFactory(ItemIndexFileHashMap map) {
-            super(map);
-        }
-
-        @Override
-        public Callable<?> create() {
-            return new Callable<Object>() {
-                @Override
-                public Object call() throws Exception {
-                    return map.remove(Md5Key.valueOf(genKey()));
-                }
-            };
-        }
-
-    }
 }
