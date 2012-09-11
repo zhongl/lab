e01f280d56a12bcb74828fd2050cf97ba5254f15
zhongl <zhong.lunfu@gmail.com>
2012-09-11 14:32:02 +0800

    Clean code.

--- codejam/src/main/scala/com/github/zhongl/codejam/CountPrimeByParFilter.scala
+++ codejam/src/main/scala/com/github/zhongl/codejam/CountPrimeByParFilter.scala
@@ -1,6 +1,6 @@
 package com.github.zhongl.codejam
 
-import java.util
+import java.util.{BitSet => Bits}
 import annotation.tailrec
 import System.{currentTimeMillis => now}
 import actors.Actor._
@@ -14,9 +14,9 @@ object CountPrimeByParFilter extends App {
     case _        => throw new IllegalArgumentException("Usage: CMD <num>")
   }
 
-  val (elapse, count) = time { countPrimeIn(num) }
-  printf("the count of primes from 1 to %1$s is: %2$s, time elapse: %3$,d ms\n", num, count, elapse)
-
+  val begin = now
+  val count = countPrimeIn(num)
+  printf("the count of primes from 1 to %1$s is: %2$s, time elapse: %3$,d ms\n", num, count, now - begin)
 
   def countPrimeIn(num: Int) = {
     val maxValidateNum = math.sqrt(num).toInt + 1
@@ -25,37 +25,24 @@ object CountPrimeByParFilter extends App {
 
     0 until parallels foreach {
       n => actor {
-        val filter = new util.BitSet(num)
-        filter.set(0) // 0 is not a prime
-        filter.set(1) // 1 is not a prime
+        val filter = new Bits(num)
+        filter.set(0, 2) // 0,1 is not a prime
 
-        @tailrec
-        def setNonPrimeByProductOf(i: Int, j: Int) {
-          i * j match {
-            case index if (index < num) => filter.set(index); setNonPrimeByProductOf(i, j + 1)
-            case _                      =>
-          }
+        @tailrec def setNonPrimeByProductOf(i: Int, j: Int): Option[_] = i * j match {
+          case index if (index < num) => filter.set(index); setNonPrimeByProductOf(i, j + 1)
+          case _                      => None
         }
+
         (2 + n) until maxValidateNum by parallels foreach { i => setNonPrimeByProductOf(i, i) }
         main ! filter
       }
-
     }
 
-    val filter: util.BitSet = new util.BitSet(num)
-    var running = true
-    var finished = 0
-    while (running) {
-      receive { case f: util.BitSet => filter or f; finished += 1; if (finished == parallels) running = false }
-    }
+    @inline def income = receive { case f: Bits => f }
 
-    num - filter.cardinality()
+    @tailrec def reduce(filter: Bits, finished: Int): Bits =
+      if (finished == parallels) filter else {filter or income; reduce(filter, finished + 1) }
+
+    num - reduce(income, 1).cardinality()
   }
-
-  private def time[T](fun: => T) = {
-    val begin = now
-    val res = fun
-    (now - begin, res)
-  }
-
 }
