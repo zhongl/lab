738f794c66da1fc793fe34dd8594be39af4ff6c9
zhongl <zhong.lunfu@gmail.com>
2012-09-25 09:28:28 +0800

    Revert ParCountAndPrintPrime

--- codejam/project/CodejamBuild.scala
+++ codejam/project/CodejamBuild.scala
@@ -11,7 +11,9 @@ object CodejamBuild extends Build {
       organization := "com.github.zhongl",
       version := "0.1-SNAPSHOT",
       scalaVersion := "2.9.2",
+      resolvers += "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/",
       libraryDependencies ++= Seq(
+        "com.typesafe.akka" % "akka-actor" % "2.0.3",
         "org.mockito" % "mockito-all" % "1.9.0" % "test",
         "org.scalatest" %% "scalatest" % "1.7.2" % "test"
       )
--- codejam/src/main/java/com/github/zhongl/codejam/ParCountAndPrintPrime.java
+++ codejam/src/main/java/com/github/zhongl/codejam/ParCountAndPrintPrime.java
@@ -4,6 +4,7 @@ import static java.lang.Integer.parseInt;
 import static java.lang.System.currentTimeMillis;
 
 import java.util.ArrayList;
+import java.util.BitSet;
 import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
@@ -86,7 +87,7 @@ public class ParCountAndPrintPrime {
 
         private final int from;
         private final int to;
-        private final FastBitSet filter;
+        private final BitSet filter;
 
         public PrimeDetector(int number) {
             this(0, number);
@@ -95,8 +96,8 @@ public class ParCountAndPrintPrime {
         public PrimeDetector(int from, int to) {
             this.from = from;
             this.to = to;
-            this.filter = new FastBitSet(to - from);
-            if (from == 0) filter.fastSet(0); // 0 is not a prime
+            this.filter = new BitSet(to - from);
+            if (from == 0) filter.set(0); // 0 is not a prime
         }
 
         @Override
@@ -111,7 +112,7 @@ public class ParCountAndPrintPrime {
                     int k = i * j;
                     if (k > to - 1) break;
                     if (k < from || j < i) continue;
-                    filter.fastSet(k - from);
+                    filter.set(k - from);
                 }
             }
         }
@@ -127,49 +128,4 @@ public class ParCountAndPrintPrime {
         }
     }
 
-    static class FastBitSet {
-        /* Used to shift left or right for a partial word mask */
-        private static final long WORD_MASK = 0xffffffffffffffffL;
-
-        protected long[] bits;
-        protected int wlen; // number of words (elements) used in the array
-
-        public FastBitSet(long numBits) {
-            bits = new long[bits2words(numBits)];
-            wlen = bits.length;
-        }
-
-        public static int bits2words(long numBits) {
-            return (int) (((numBits - 1) >>> 6) + 1);
-        }
-
-        public void fastSet(int index) {
-            int wordNum = index >> 6; // div 64
-            int bit = index & 0x3f; // mod 64
-            long bitmask = 1L << bit;
-            bits[wordNum] |= bitmask;
-        }
-
-        public int cardinality() {
-            int sum = 0;
-            for (int i = 0; i < wlen; i++)
-                sum = Long.bitCount(bits[i]);
-            return sum;
-        }
-
-        public int nextClearBit(int index) {
-            int i = index >> 6;
-            if (i >= wlen)
-                return index;
-
-            long word = ~bits[i] & (WORD_MASK << index);
-            while (true) {
-                if (word != 0)
-                    return (i << 6) + Long.numberOfTrailingZeros(word);
-                if (++i == wlen)
-                    return wlen << 6;
-                word = ~bits[i];
-            }
-        }
-    }
 }
